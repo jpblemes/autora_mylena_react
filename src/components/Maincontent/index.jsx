d3bc8a04b11d91da52fe96137bde21753e3fd8bf
João Paulo Borges Lemes <jpblemes@hotmail.com>
2021-05-17 07:55:43 -0300

    feat: Added 'Hazel' book Amazon url.

--- src/components/Maincontent/index.jsx
+++ src/components/Maincontent/index.jsx
@@ -1,69 +1,20 @@
 import React from "react";
-import { hazelPreVenda } from "./styles";
+import { hazelPreVenda, amazonurl } from "./styles";
 
 const Maincontent = () => {
   return (
-    <div className="ui container">
-      <div>
-        <img
-          className="ui centered image"
-          src={hazelPreVenda.src}
-          alt={hazelPreVenda.alt}
-        />
-      </div>
-      {/* <div style={bgstyle} className="ui fluid card">
-        <div className="content">
-          <br />
-          <br />
-          <div className="description">
-            <div className="ui right floated vertical buttons bookmenu">
-              <img
-                className="marginPhoto small ui image"
-                src={phone}
-                alt="Smartphone with the book on screen"
-              />
-            </div>
-            <div>
-              <div style={titlestyle} className="header">
-                <h2 style={{ color: "#F8F8FF" }}>A Rebelde de Dois Mundos</h2>
-              </div>
-              <br />
-              <br />
-              <div style={textstyle}>
-                <p style={{ color: "#F8F8FF" }}>
-                  Uma bela de uma Ladra convencida teve o seu destino roubado de
-                  suas mãos.
-                </p>
-                <p style={{ color: "#F8F8FF" }}>
-                  Áquila não é quem diz ser e Isabelle não é quem pensou que
-                  fosse.
-                </p>
-                <p style={{ color: "#F8F8FF" }}>
-                  Nessa luta de opostos, quem irá vencer: A honra ou o amor?
-                </p>
-              </div>
-              <br />
-              <div
-                style={{ backgroundColor: "#cbbce6" }}
-                className="ui centered card"
-              >
-                <div className="content">
-                  <div className="header">Adquira o e-book!</div>
-                  <br />
-                  <a target="_blank" href={amazonurl} rel="noreferrer">
-                    <button className="ui violet button">
-                      <i className="circular inverted amazon icon"></i>
-                      Amazon BR
-                    </button>
-                  </a>
-                </div>
-              </div>
-            </div>
-            <br />
-            <br />
-          </div>
-        </div>
-      </div> */}
+    <div style={{ marginTop: "30px" }} className="ui container">
+      <img
+        className="ui centered image"
+        src={hazelPreVenda.src}
+        alt={hazelPreVenda.alt}
+      />
+      <a href={amazonurl} target="_blank" rel="noreferrer">
+        <button className="ui violet button">
+          <i className="circular inverted amazon icon"></i>
+          Adquira o seu na Pré-Venda!
+        </button>
+      </a>
     </div>
   );
 };
--- src/components/Maincontent/styles.js
+++ src/components/Maincontent/styles.js
@@ -3,8 +3,7 @@ export const hazelPreVenda = {
   alt: "Pré venda do livro Hazel. 27 de Maio na Amazon. Grátis com Kindle Unlimited.",
 };
 
-export const amazonurl =
-  "https://www.amazon.com.br/Rebelde-Dois-Mundos-convencida-destino-ebook/dp/B08P94FNF3";
+export const amazonurl = "https://amzn.to/3tSJ3JV";
 export const bgimg = "../images/background.png";
 export const bgstyle = {
   backgroundImage: `url(${bgimg})`,
